fae753b28b9b6c7beb9a303d9bc515e6773a0822
ruybragafilho <ruybragafilho@gmail.com>
2021-11-29 03:05:34 -0300

    Invremento no modulo main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 #include "TipoLista.h"
 
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -23,10 +25,28 @@ void carregarDimensoesGrid( long& n, long& m );
 void carregarLojas( TipoLista<Loja>& loja );
 
 
+/* Prototipo da função que carrega os clientes */
+void carregarClientes( TipoLista<Cliente>& cliente );
+
+
+/* Prototipo da função que calcula o score_estado */
+long scoreEstado( string& uf );
+
+
+/* Prototipo da função que calcula o score_tipo_pagamento */
+long scoreTipoPagamento( string& u );
+
+
+/* Prototipo da função que carrega os clientes */
+double calcularTicket( long scoreEstado,
+                       long scoreTipoPagamento,
+                       long idade );
+
 
 
 /* Definição da função main */
 int main()  {
+
 
     /* Dimensões do grid */
     long n;
@@ -35,9 +55,20 @@ int main()  {
     cout << "\n\nN: " << n;
     cout << "\nM: " << m << "\n\n"; 
 
+
     /* Lojas */
     TipoLista<Loja> loja;
-    loja.mostrarLista();
+    carregarLojas( loja );
+    loja.imprimir();
+    cout << "\n\nCarregou lojas\n\n";   
+
+
+    /* Clientes */
+    TipoLista<Cliente> cliente;
+    carregarClientes( cliente );
+    cliente.imprimir();
+    cout << "\n\nCarregou clientes\n\n";   
+
 
     return 0;
 
@@ -79,7 +110,84 @@ void carregarLojas( TipoLista<Loja>& loja )  {
         Loja l( i, estoqueLoja, p );
 
         loja.inserirNoFim( l );
-    }
-    
+    }    
 
-}  /* Fim da definição da função que carrega as lojas */
+}  /* Fim da definição da função carregarLojas */
+
+
+
+/* Definição da função que carrega os clientes */
+void carregarClientes( TipoLista<Cliente>& cliente )  {
+
+    long numClientes;
+
+    long idade;
+    string uf;
+    string u;
+    long x;
+    long y; 
+
+    // Carrega o número de lojas
+    cin >> numClientes;
+
+    // Carrega os dados de cada loja
+    for( long i=0; i<numClientes; ++i )  {
+
+        cin >> idade;
+        cin >> uf;
+        cin >> u;
+        cin >> x;
+        cin >> y;
+
+        Ponto2D p(x, y);
+        Cliente c( i, 
+                   calcularTicket( scoreEstado( uf ),
+                                   scoreTipoPagamento( u ),
+                                   idade ), 
+                   p );
+
+        cliente.inserirNoFim( c );
+    }    
+
+}  /* Fim da definição da função carregarClientes */
+
+
+
+/* Definição da função que calcula o score_estado */
+long scoreEstado( string& uf )  {
+
+    if( uf == "MG" ) return 0;
+    if( uf == "PR" ) return 10;
+    if( uf == "SP" ) return 20;
+    if( uf == "SC" ) return 30;
+    if( uf == "RJ" ) return 40;
+    if( uf == "RN" ) return 50;
+    if( uf == "RS" ) return 60;
+
+    return -1;
+
+}  /* Fim da definição da função scoreEstado */
+
+
+
+/* Definição da função que calcula o score_tipo_pagamento */
+long scoreTipoPagamento( string& u )  {
+
+    if( u == "DINHEIRO" ) return 1;
+    if( u == "DEBITO" )   return 2;
+    if( u == "CREDITO" )  return 3;    
+
+    return -1;
+
+}  /* Fim da definição da função scoreTipoPagamento */
+
+
+
+/* Definição da função que carrega os clientes */
+double calcularTicket( long scoreEstado,
+                       long scoreTipoPagamento,
+                       long idade )  {
+
+    return ( abs(60 - idade) + scoreEstado) / static_cast<double>(scoreTipoPagamento);
+
+}  /* Fim da definição da função calcularTicket */
